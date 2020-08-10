a3d11031c89c3286ca04086484f2537a0cc0983b
Adrian Schaedle <adrian@schaedle.me>
2020-08-09 23:22:19 -0400

    Copy checkboxes Javascript into theme

---
+++ rollup.config.js
@@ -0,0 +1,32 @@
+import svelte from "rollup-plugin-svelte";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import { terser } from "rollup-plugin-terser";
+import replace from "@rollup/plugin-replace";
+import filesize from "rollup-plugin-bundle-size";
+import typescript from "@rollup/plugin-typescript";
+import autoPreprocess from "svelte-preprocess";
+
+const isProduction = !process.env.ROLLUP_WATCH;
+
+const plugins = [
+  svelte({ dev: !isProduction, preprocess: autoPreprocess() }),
+  nodeResolve({ browser: true, dedupe: ["svelte"] }),
+  commonjs(),
+  typescript({ sourceMap: !isProduction })
+];
+
+if (isProduction) {
+  plugins.push(terser(), filesize());
+}
+
+export default {
+  input: "js/index.ts",
+  output: {
+    name: "checkboxes",
+    file: "assets/js/bundle.js",
+    format: "iife",
+    sourcemap: true
+  },
+  plugins
+};
